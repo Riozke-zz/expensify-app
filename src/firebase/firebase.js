4542c5287041da6c24c2a98aa195962a1b19ff5e
Joestar <jdanny.cobos96@gmail.com>
2020-01-09 19:11:26 -0600

    Setup database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,18 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBlLxi9KYuwsjj3vOtgy8CkWD2Rg5gj0JQ",
+  authDomain: "expensify-da9bb.firebaseapp.com",
+  databaseURL: "https://expensify-da9bb.firebaseio.com",
+  projectId: "expensify-da9bb",
+  storageBucket: "expensify-da9bb.appspot.com",
+  messagingSenderId: "825831820949",
+  appId: "1:825831820949:web:9e8914993e96615554959c",
+  measurementId: "G-60NT3B13QT"
 };
 
 firebase.initializeApp(config);
-//firebase.analytics();
+firebase.analytics();
 
 const database = firebase.database();
 
